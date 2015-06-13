a0fd7812669d4e584e8d16260344e0ada18cb6d3
Michael Pope <map7@corsairsolutions.com.au>
2015-06-13 18:52:26 +1000

    Split out to a variable

--- lib/action_view/bruce/bruce.rb
+++ lib/action_view/bruce/bruce.rb
@@ -6,12 +6,13 @@ module Bruce
       # <%= bruce %>
       #
       def bruce
+        @percent = 50
         # Render the image
         body = content_tag(:div, image_tag("ozemade.png"))
 
         # Render the bar graph with the percentage of Australian Gems in your project
         # Use the style width to show a percentage
-        body += "<div class='bar'><div class='percent' style='width:70%'></div></div>".html_safe
+        body += "<div class='bar'><div class='percent' style='width:#{@percent}%'></div></div>".html_safe
 
         # Wrap everything in a div
         content_tag(:div, body)
