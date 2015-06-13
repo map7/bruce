617cfe5c5087fe0f816ace7cbe36891cb2f66696
Michael Pope <map7@corsairsolutions.com.au>
2015-06-13 16:32:40 +1000

    Add more to the render

--- lib/action_view/bruce/bruce.rb
+++ lib/action_view/bruce/bruce.rb
@@ -1,9 +1,20 @@
 module Bruce
   module ::ActionView
     module Helpers
+      #
+      # Call the bruce function within your views like so
+      # <%= bruce %>
+      #
       def bruce
-        # "BRUCE has landed"
-        image_tag("ozemade.png")
+        # Render the image
+        body = content_tag(:div, image_tag("ozemade.png"))
+
+        # Render the bar graph with the percentage of Australian Gems in your project
+        # Use the style width to show a percentage
+        body += "[+++--]" 
+
+        # Wrap everything in a div
+        content_tag(:div, body)
       end
     end
   end
