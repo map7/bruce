325f61a2ebc7523750c34ab8655f103072dc05ff
Michael Pope <map7@corsairsolutions.com.au>
2015-06-13 17:58:09 +1000

    Add comments

--- lib/generators/bruce/install_generator.rb
+++ lib/generators/bruce/install_generator.rb
@@ -5,6 +5,7 @@ module Bruce
       desc "Install bruce stylesheets to your application."
 
       def install_stylesheets
+        # Put 'require bruce' just above the require_tree in the application css file.
         line = " *= require_tree ."
         gsub_file 'app/assets/stylesheets/application.css', /(#{Regexp.escape(line)})/mi do |match|
           " *= require bruce\n#{match}"
