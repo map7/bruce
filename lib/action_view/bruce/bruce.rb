c87e18b329b3efc3e743e019701769a61af095a0
Michael Pope <map7@corsairsolutions.com.au>
2015-06-14 01:42:01 +1000

    remove the need for :: as this means take it back to the base class

--- lib/action_view/bruce/bruce.rb
+++ lib/action_view/bruce/bruce.rb
@@ -1,27 +1,26 @@
-module Bruce
-  module ::ActionView
-    module Helpers
-      #
-      # Call the bruce function within your views like so
-      # <%= bruce %>
-      #
-      def bruce
-        # Get the percentage. 
-        config= ActionView::Config.new
-        config.load
-        
-        # Render the image
-        body = content_tag(:div, image_tag("ozemade.png"))
+module ActionView
+  module Helpers
+    #
+    # Call the bruce function within your views like so
+    # <%= bruce %>
+    #
+    def bruce
+      # Get the percentage. 
+      config= ActionView::Config.new
+      config.load
+      
+      # Render the image
+      body = content_tag(:div, image_tag("ozemade.png"))
 
-        # Render the bar graph with the percentage of Australian Gems in your project
-        # Use the style width to show a percentage
-        body += "<div class='bar'><div class='percent' style='width:#{config.percent}%'></div></div>".html_safe
+      # Render the bar graph with the percentage of Australian Gems in your project
+      # Use the style width to show a percentage
+      body += "<div class='bar'><div class='percent' style='width:#{config.percent}%'></div></div>".html_safe
 
-        body += "<div class='desc'>#{config.percent}% Proudly Australian</div>".html_safe
+      body += "<div class='desc'>#{config.percent}% Proudly Australian</div>".html_safe
 
-        # Wrap everything in a div
-        content_tag(:div, body, class: 'box')
-      end
+      # Wrap everything in a div
+      content_tag(:div, body, class: 'box')
     end
   end
 end
+
