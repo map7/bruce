d305e1b50f21b90cee7a0fdbdc0ab0fa8d7c4caa
Michael Pope <map7@corsairsolutions.com.au>
2015-06-14 16:21:58 +1000

    Add a few more kanga options

--- lib/action_view/bruce/bruce.rb
+++ lib/action_view/bruce/bruce.rb
@@ -6,20 +6,30 @@ module ActionView
     #
     def bruce
       # Get the percentage. 
-      config= ::Bruce::Config.new
-      config.load
+      @config= ::Bruce::Config.new
+      @config.load
       
       # Render the image
-      body = content_tag(:div, image_tag("ozemade.png"))
+      body = content_tag(:div, image_tag(kanga))
 
       # Render the bar graph with the percentage of Australian Gems in your project
       # Use the style width to show a percentage
-      body += "<div class='bar'><div class='percent' style='width:#{config.percent}%'></div></div>".html_safe
+      body += "<div class='bar'><div class='percent' style='width:#{@config.percent}%'></div></div>".html_safe
 
-      body += "<div class='desc'>#{config.percent}% Proudly Australian</div>".html_safe
+      body += "<div class='desc'>#{@config.percent}% Proudly Australian</div>".html_safe
 
       # Wrap everything in a div
       content_tag(:div, body, class: 'box')
+    end
+
+    def kanga
+      if @config.percent <= 20
+        "ozemade-dead.png"
+      elsif @config.percent >= 80
+        "ozemade-cool.png"
+      else
+        "ozemade.png"
+      end        
     end
   end
 end
