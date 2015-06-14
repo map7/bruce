7103146ee3feee6386a5175b86e500fe9dfeb639
Michael Pope <map7@corsairsolutions.com.au>
2015-06-14 11:39:19 +1000

    Fix version test

--- spec/bruce_spec.rb
+++ spec/bruce_spec.rb
@@ -1,11 +1,7 @@
-require 'spec_helper'
+# require 'spec_helper'
 
-describe Bruce do
+describe ActionView::Bruce do
   it 'has a version number' do
-    expect(Bruce::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
+    expect(ActionView::Bruce::VERSION).not_to be nil
   end
 end
